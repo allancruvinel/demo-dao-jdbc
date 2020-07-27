860124c84e6470f8c77a9a6b2d88ec557c319d86
Allan Cruvinel <allan.cruvinel@gmail.com>
2020-07-27 15:38:32 -0300

    reusando a instanciacao

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,19 +1,18 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
+
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
+		System.out.println("=== TEST 1: seller finById ====");
 		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
